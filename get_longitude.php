e1445e5d474c3a8793f7358db56b96ac6878f036
otero <supwer00@gmail.com>
2018-01-31 18:43:38 +0100

    Update get_longitude.php

--- get_longitude.php
+++ get_longitude.php
@@ -1,19 +1,22 @@
 <?php
-    // Prepare variables for database connection
-    $dbusername = "fbpmf";  // enter database username, I used "arduino" in step 2.2
-    $dbpassword = "Werominecon2017";  // enter database password, I used "arduinotest" in step 2.2
-    $server = "193.70.86.126"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
-    $My_db = "itemtracker";
-    // Connect to your database
+    // Peparació de les variables per la conexió a la base de dades
+    $dbusername = "fbpmf";  // usuari
+    $dbpassword = "Werominecon2017";  // contrasenya
+    $server = "193.70.86.126"; // servidor
+    $My_db = "itemtracker"; // nom de la base de dades
+    // Connexió a la base de dades
     $dbconnect = mysql_pconnect($server, $dbusername, $dbpassword);
     $dbselect = mysql_select_db("itemtracker",$dbconnect);
-    // Prepare the SQL statement
+    // Preparació de la declaració SQL
     $sql = "select longitude, latitude from itemtracker_locators where secretkey =  '".$_GET["key"]."'";
-    // Execute SQL statement
+    // Executem la declaració SQL
     $result = mysql_query($sql);
-	$row = mysql_fetch_array($result);
-	$longitude = $row[0];
-	$latitude = $row[1];
-	echo($longitude);
-	mysql_close($dbconnect);
+    // Seleccionem les dades que necessitem de la base de dades
+    $row = mysql_fetch_array($result);
+    $longitude = $row[0];
+    $latitude = $row[1];
+    // Enviem una resposta amb la longitud
+    echo($longitude);
+    // Tanquem la connexió amb la base de dades
+    mysql_close($dbconnect);
 ?>
